6a8dd6ef01fb2fba62184787dc6c5cee84620c3e
Eldad Fux <eldad.fux@gmail.com>
2020-08-14 13:52:52 +0300

    Added tests

---
+++ tests/Preloader/PreloaderTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Utopia\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Utopia\Preloader\Preloader;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+class PreloaderTest extends TestCase
+{
+    public function setUp()
+    {
+        
+    }
+
+    public function tearDown()
+    {
+        $this->test = null;
+    }
+
+    public function testTest()
+    {
+        $preloader = new Preloader();
+        
+        $preloader
+            //->paths(realpath(__DIR__ . '/../resources'))
+            // ->paths(realpath(__DIR__ . '/../resources/nested'))
+            ->load();
+
+        $autoloaded = $preloader->getCount();
+
+        $this->assertGreaterThan(500, $autoloaded);
+        
+        $preloader = new Preloader();
+        
+        $preloader
+            ->paths(realpath(__DIR__ . '/../resources'))
+            ->load();
+        
+        $this->assertEquals($autoloaded + 3, $preloader->getCount());
+        
+        $preloader
+            ->paths(realpath(__DIR__ . '/../resources'))
+            ->ignore(realpath(__DIR__ . '/../resources/nested'))
+            ->load();
+        
+        $this->assertEquals($autoloaded + 2, $preloader->getCount());
+    }
+}
